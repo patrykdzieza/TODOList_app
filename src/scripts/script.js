85549588d24fcaf87d337626f2d5fe9de0a8423e
Patryk <patryk.dzieza@creatorshub.pl>
2023-08-01 15:41:34 +0200

    Added script.js to index

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,23 +1,27 @@
-let newTaskInput
-let errorInfo
-let addNewTaskButton
-let taskList
-
+let newTaskInput;
+let errorInfo;
+let addNewTaskButton;
+let taskList;
+let task;
+let removeTaskButton;
 function main() {
-    prepareDOMElements();
-    prepareDOMEvents();
+  prepareDOMElements();
+  prepareDOMEvents();
 }
 
-function prepareDOMElements(){
-    document.querySelector(".new-task__input-field")
-    document.querySelector(".new-task__add-button")
-    document.querySelector("ul.task-list")
-
+function prepareDOMElements() {
+  document.querySelector('.new-task__input-field');
+  addNewTaskButton = document.querySelector('.new-task__add-button');
+  document.querySelector('ul.task-list');
+  task = document.querySelector('.task-list__item--done');
+  removeTaskButton = document.querySelector('.task-done-icon');
 }
 
-function prepareDOMEvents(){
+function prepareDOMEvents() {}
 
+function hidding() {
+  task.classList.toggle('hidden');
 }
 
-document.addEventListener("DOMContentLoaded", main)
-
+// removeTaskButton.addEventListener("click", hidding)
+document.addEventListener('DOMContentLoaded', main);
